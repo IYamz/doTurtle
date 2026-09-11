188ec7c0bf97b143d8c014cbca451804ea192d7a
Yamz <70824779+IYamz@users.noreply.github.com>
2026-09-11 13:31:08 -0400

    Create doTurtleHost.lua

---
+++ doTurtleHost.lua
@@ -0,0 +1,24 @@
+local appName = "doTurtle"
+local version = "1.0.0-alpha.01"
+
+local commands = { -- Table of commands
+  {name = "help",
+    description = "Lists commands",
+    func = function()
+      for i,command in pairs(commands) do
+        print(command.name .. " - " .. description)
+      end
+    end
+  }
+}
+
+local function clear() -- Clears the screen
+  term.clear()
+  term.setCursorPos(1,1)
+end
+
+clear()
+write(appName .. " " .. version)
+term.setCursorPos(1,3)
+write("Use 'help' for a list of commands")
+term.setCursorPos(1, 5)
